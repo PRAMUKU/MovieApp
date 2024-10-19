4777d632a1fe2d7a4661d23fcf0f815bc810d293
Pramuk U <pramuk.u1@viatris.com>
2024-10-19 18:38:54 +0530

    Initial Commit

---
+++ MovieApp/MovieAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieAppApp.swift
+//  MovieApp
+//
+//  Created by Pramuk.u on 19/10/24.
+//
+
+import SwiftUI
+
+@main
+struct MovieAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
