f5577a8f33a3d6ac8d37ec152499d3a8aa98c024
Pramuk U <pramuk.u1@viatris.com>
2024-10-20 22:00:29 +0530

    Movie detail view implementation

---
+++ MovieApp/Features/MovieDetail/View/DetailCellView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailCellView.swift
+//  MovieApp
+//
+//  Created by Pramuk.u on 20/10/24.
+//
+
+import SwiftUI
+
+struct DetailCellView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailCellView()
+}
