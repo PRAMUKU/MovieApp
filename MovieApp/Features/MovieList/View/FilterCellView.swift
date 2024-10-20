259edc2e4a3bc001ff5a584f871fdfd773387383
Pramuk U <pramuk.u1@viatris.com>
2024-10-20 21:01:15 +0530

    Movie list screen implementation.

---
+++ MovieApp/Features/MovieList/View/FilterCellView.swift
@@ -0,0 +1,18 @@
+//
+//  FilterCellView.swift
+//  MovieApp
+//
+//  Created by Pramuk.u on 20/10/24.
+//
+
+import SwiftUI
+
+struct FilterCellView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FilterCellView()
+}
---
+++ MovieApp/Features/MovieList/View/MovieCellView.swift
@@ -0,0 +1,18 @@
+//
+//  MovieCellView.swift
+//  MovieApp
+//
+//  Created by Pramuk.u on 20/10/24.
+//
+
+import SwiftUI
+
+struct MovieCellView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MovieCellView()
+}
