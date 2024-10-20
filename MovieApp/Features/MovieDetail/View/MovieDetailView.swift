70857ea9a5f1cc0fda1a5838ad08038bdc1c4da1
Pramuk U <pramuk.u1@viatris.com>
2024-10-20 06:49:57 +0530

    Basic files created and organised

---
+++ MovieApp/Features/MovieDetail/View/MovieDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  MovieDetailView.swift
+//  MovieApp
+//
+//  Created by Pramuk.u on 20/10/24.
+//
+
+import SwiftUI
+
+struct MovieDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MovieDetailView()
+}
