0b397cbb0de3d35c6d3fb143ced61eadafe4c99b
Pramuk U <pramuk.u1@viatris.com>
2024-10-21 00:24:56 +0530

    Rating detail view implemented

---
+++ MovieApp/Features/MovieDetail/View/RatingDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  RatingDetailView.swift
+//  MovieApp
+//
+//  Created by Pramuk.u on 20/10/24.
+//
+
+import SwiftUI
+
+struct RatingDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RatingDetailView()
+}
